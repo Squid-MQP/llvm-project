df896b563addc5a3a61f6981dc84e8797a51f871
Robin Husman <rlhusman@wpi.edu>
2026-02-24 17:44:40 -0500

    This is a successful build with attaching metadata

--- llvm/lib/Transforms/Utils/MyPass.cpp
+++ llvm/lib/Transforms/Utils/MyPass.cpp
@@ -1,9 +1,15 @@
 #include "llvm/Transforms/Utils/MyPass.h"
+#include "llvm/IR/Function.h"
 
 using namespace llvm;
 
 PreservedAnalyses MyPass::run(Function &F,
                                       FunctionAnalysisManager &AM) {
+  for (auto&& bb : F) {
+    for (auto&& inst : bb) {
+      inst.addAnnotationMetadata(StringRef{"test"});
+    }
+  }
   errs() << F.getName() << "\n";
-  return PreservedAnalyses::all();
+  return PreservedAnalyses::none();
 }
