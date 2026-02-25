cf2f833e8358ad751d81f7f079d760fb3d434970
Robin Husman <rlhusman@wpi.edu>
2026-02-24 19:57:42 -0500

    Basic block floating and sinking build and run.

--- llvm/lib/Transforms/Utils/MyPass.cpp
+++ llvm/lib/Transforms/Utils/MyPass.cpp
@@ -1,14 +1,51 @@
 #include "llvm/Transforms/Utils/MyPass.h"
 #include "llvm/IR/Function.h"
+#include <vector>
 
 using namespace llvm;
 
 PreservedAnalyses MyPass::run(Function &F,
                                       FunctionAnalysisManager &AM) {
   for (auto&& bb : F) {
+    std::vector<llvm::Instruction*> toFloat;
+    std::vector<llvm::Instruction*> toSink;
     for (auto&& inst : bb) {
-      inst.addAnnotationMetadata(StringRef{"test"});
+      if (auto* mdnode = inst.getMetadata(LLVMContext::MD_annotation)) {
+        auto* tupl = cast<MDTuple>(mdnode);
+        for (const llvm::MDOperand& arg : tupl->operands()) {
+          if (isa<MDString>(arg)) {
+            auto stringed = cast<MDString>(arg);
+
+            auto name = stringed->getString().str();
+            if (name == "Float") {
+              errs() << "Recording for float!\n";
+              toFloat.push_back(&inst);
+            }
+            else if (name == "Sink") {
+              errs() << "Recording for sink!\n";
+              toSink.push_back(&inst);
+            }
+
+            //errs() << "v2: " << stringed->getString().str() << "\n";
+          }
+        }
+      }
+      else {
+        std::string mystr = "test" + std::string{inst.getOpcodeName()};
+      inst.addAnnotationMetadata(StringRef{mystr});
+      errs() << "Applying annotation" << "\n";
+      }
     }
+
+    
+    for (auto itr{toFloat.rbegin()}; itr != toFloat.rend(); ++itr) {
+      (*itr)->moveBefore(bb.begin());
+    }
+
+    for (auto&& sinkMe : toSink) {
+      sinkMe->moveBefore(--bb.end());
+    }
+      
   }
   errs() << F.getName() << "\n";
   return PreservedAnalyses::none();
